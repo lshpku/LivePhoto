f784be387b8ab6f67517781254df8c6d3be348e4
tonywyb <1500012946@pku.edu.cn>
2019-06-02 13:59:17 +0800

    add init

--- src/DBInterface/init.java
+++ src/DBInterface/init.java
@@ -8,20 +8,20 @@ public class init {
 
     public static void main(String[] args) {
         for(int i = 0; i < 5; ++i){
-            DBInterface.createAccount("test" + i);
+            createAccount("test" + i);
             String theme = "UNK_" + i;
             String description = "UNK news for test" + i;
             String account = "test" + i;
             SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             String time = df.format(new Date());
             String content = "theme=" + theme + "description=" + description + "account=" + account + "time=" + time;
-            DBInterface.establishTheme(content);
-//            DBInterface.deleteTheme(i);
+            establishTheme(content);
+//            deleteTheme(i);
         }
         for (int i = 0; i < 5; ++i){
             String photo = null;
             String file_path = "../WebServer/demo";
-            byte[] byte_1 = DBInterface.getBytes(file_path + "1000" + i);
+            byte[] byte_1 = getBytes(file_path + "1000" + i);
             try{
                 photo = new String(byte_1,"ISO-8859-1");}
             catch(java.io.UnsupportedEncodingException e){
@@ -30,11 +30,11 @@ public class init {
             String account = "test" + i;
             String word = "UNK photo for test" + i;
             String content = "account=" + account + "num=" + i + "word=" + word + "photo=" + photo;
-            DBInterface.sendInfo(content);
-//            DBInterface.readDB2Image("../WebServer/demoResult", i + ".jpg", i);
+            sendInfo(content);
+//            readDB2Image("../WebServer/demoResult", i + ".jpg", i);
         }
-//        DBInterface.getFile(DBInterface.getPhoto(2), "../WebServer/demoResult", "test.jpg");
-        News n = DBInterface.getNews(2);
-        News[] n_lis = DBInterface.getRecentNews(3);
+//        getFile(getPhoto(2), "../WebServer/demoResult", "test.jpg");
+        News n = getNews(2);
+        News[] n_lis = getRecentNews(3);
     }
 }
