5b20f37caac129e1e1fb2527c7c77fcb6dd52b5d
Shuhao Liang <50269654+lshpku@users.noreply.github.com>
2019-06-02 13:35:11 +0800

    Add files via upload

---
+++ src/LivePhoto.java
@@ -0,0 +1,9 @@
+import WebServer.WebServer;
+
+public class LivePhoto {
+
+    public static void main(String[] argv) {
+        System.out.println("LivePhoto start...");
+        new WebServer().start();
+    }
+}
