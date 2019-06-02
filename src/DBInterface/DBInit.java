158f4808c79f4e508278fc65498d8b0c379c3c0f
tonywyb <1500012946@pku.edu.cn>
2019-06-02 14:39:22 +0800

    debug complete

--- src/DBInterface/DBInit.java
+++ src/DBInterface/DBInit.java
@@ -8,20 +8,20 @@ public class DBInit {
 
     public static void main(String[] args) {
         for(int i = 0; i < 5; ++i){
-            createAccount("test" + i);
+            DBInterface.createAccount("test" + i);
             String theme = "UNK_" + i;
             String description = "UNK news for test" + i;
             String account = "test" + i;
             SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             String time = df.format(new Date());
             String content = "theme=" + theme + "description=" + description + "account=" + account + "time=" + time;
-            establishTheme(content);
+            DBInterface.establishTheme(content);
 //            deleteTheme(i);
         }
         for (int i = 0; i < 5; ++i){
             String photo = null;
             String file_path = "../WebServer/demo";
-            byte[] byte_1 = getBytes(file_path + "1000" + i);
+            byte[] byte_1 = DBInterface.getBytes(file_path + "1000" + i);
             try{
                 photo = new String(byte_1,"ISO-8859-1");}
             catch(java.io.UnsupportedEncodingException e){
@@ -30,11 +30,11 @@ public class DBInit {
             String account = "test" + i;
             String word = "UNK photo for test" + i;
             String content = "account=" + account + "num=" + i + "word=" + word + "photo=" + photo;
-            sendInfo(content);
+            DBInterface.sendInfo(content);
 //            readDB2Image("../WebServer/demoResult", i + ".jpg", i);
         }
 //        getFile(getPhoto(2), "../WebServer/demoResult", "test.jpg");
-        News n = getNews(2);
-        News[] n_lis = getRecentNews(3);
+        News n = DBInterface.getNews(2);
+        News[] n_lis = DBInterface.getRecentNews(3);
     }
 }
