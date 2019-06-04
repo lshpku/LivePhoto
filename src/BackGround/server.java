bacd0072f8ba348d07766666ec7bc958ae49f636
tonywyb <1500012946@pku.edu.cn>
2019-06-04 13:09:18 +0800

    debug

--- src/BackGround/server.java
+++ src/BackGround/server.java
@@ -115,12 +115,11 @@ public class server extends Thread {
 						}
 						else {//�ϴ�ͼƬ���֣������ݷ��͸����ݿ�
 							System.out.printf("start sending photo");
-//							FileOutputStream fout1=new FileOutputStream("src/BackGround/test.jpg");
-//							while((len=is.read(Array))!=-1) {
-//								fout1.write(Array);
-//							}
-//							fout1.close();
-							DBInterface.getFile(Array, "src/BackGround", "test.jpg");
+							FileOutputStream fout1=new FileOutputStream("src/BackGround/test.jpg");
+							while((len=is.read(Array))!=-1) {
+								fout1.write(Array, 0, len);
+							}
+							fout1.close();
 							System.out.println("complete writing photo");
 							FileInputStream fin=new FileInputStream(new File("src/BackGround/test.jpg"));
 							byte[] bytes=new byte[fin.available()];
